cf40f4beb748eeebefe1bd5bfc1e485114e30633
buddybuild <help@buddybuild.com>
2017-02-15 18:20:30 -0800

    Add failing test

---
+++ app/src/test/java/com/buddybuild/bbandroidsampletests/FailUnitTest.java
@@ -0,0 +1,17 @@
+package com.buddybuild.bbandroidsampletests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class FailUnitTest {
+    @Test
+    public void assert_IsNotCorrect() throws Exception {
+        assertEquals(4, 3);
+    }
+}
